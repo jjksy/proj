ba8e4753ef2f12503046fc6438bf13dee3ac1313
reina <reina@inucreative.com>
2023-07-28 10:33:47 +0900

    chorer : proj 초기 세팅

---
+++ src/main/java/com/proj/proj/controller/BaseController.java
@@ -0,0 +1,14 @@
+package com.proj.proj.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import shop.pingping2.board.config.auth.SessionUser;
+
+import javax.servlet.http.HttpSession;
+@RequiredArgsConstructor
+@Controller
+public class BaseController{
+
+}
